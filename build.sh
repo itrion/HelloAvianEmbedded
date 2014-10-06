49c345a7ec0d70697a7d8a8ab349b7486e575059
Johan Sebastian Cortes Montenegro <johanscm50@gmail.com>
2014-10-06 09:29:35 +0100

    loading the generated library generates:
    java/lang/ExceptionInInitializerError
      at java/util/ArrayList.add (line 86)
      at java/util/ArrayList.add (line 94)
      at Hello$FooCLass.addMessage (line 29)
      at Hello.execute (line 10)
      at Hello.main (line 5)
    caused by: java/lang/ExceptionInInitializerError
      at java/util/Random.<init> (line 28)
      at java/lang/Math.<clinit> (line 18)
      at java/util/ArrayList.add (line 86)
      at java/util/ArrayList.add (line 94)
      at Hello$FooCLass.addMessage (line 29)
      at Hello.execute (line 10)
      at Hello.main (line 5)
    caused by: java/lang/UnsatisfiedLinkError: java/lang/System.currentTimeMillis()J
      at java/lang/System.currentTimeMillis (native)
      at java/lang/System.<clinit> (line 25)
      at java/util/Random.<init> (line 28)
      at java/lang/Math.<clinit> (line 18)
      at java/util/ArrayList.add (line 86)
      at java/util/ArrayList.add (line 94)
      at Hello$FooCLass.addMessage (line 29)
      at Hello.execute (line 10)
      at Hello.main (line 5)

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 rm *.o
-rm boot.jar
 rm *.class
-
+rm *.dylib
+rm hello
+rm EmbeddedAppLoader
 
 echo "############## compiling ############"
 echo
@@ -10,6 +11,8 @@ rm boot.jar
 cp classpath.jar boot.jar
 javac -bootclasspath boot.jar Hello.java
 jar u0f boot.jar *.class
+
+./binaryToObject boot.jar boot-jar.o _binary_boot_jar_start _binary_boot_jar_end darwin x86_64
 
 g++ -fno-exceptions -fno-rtti -I"$JAVA_HOME/include" -I"$JAVA_HOME/include/darwin" -D_JNI_IMPLEMENTATION_ -c embedded-jar-main.cpp -o main.o
 
